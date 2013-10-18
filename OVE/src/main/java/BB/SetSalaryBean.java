a134097feb09e300574bd3a1c6e53156697d01c0
gdahl92 <kgldahl@gmail.com>
2013-10-18 08:54:40 +0200

    The admin can now set salary for each tutor

--- OVE/src/main/java/BB/SetSalaryBean.java
+++ OVE/src/main/java/BB/SetSalaryBean.java
@@ -8,6 +8,8 @@ import EJB.WorkerRegistry;
 import Model.Worker;
 import java.io.Serializable;
 import java.util.ArrayList;
+import java.util.List;
+import javax.annotation.PostConstruct;
 import javax.ejb.EJB;
 import javax.enterprise.context.RequestScoped;
 import javax.enterprise.context.SessionScoped;
@@ -15,6 +17,7 @@ import javax.faces.application.FacesMessage;
 import javax.faces.context.FacesContext;
 import javax.inject.Named;
 import org.primefaces.context.RequestContext;
+import org.primefaces.event.CellEditEvent;
 import org.primefaces.event.RowEditEvent;
 
 /**
@@ -27,15 +30,23 @@ public class SetSalaryBean implements Serializable
 {
     @EJB
     private WorkerRegistry workerReg;
-        
+    
+    private Worker w;
+    private int newSalary;
+    private ArrayList<Worker> workerList;
+    
     public SetSalaryBean()
     {
       
     }
-    
+    @PostConstruct
+    public void postConstruct()
+    {
+     workerList = new ArrayList<Worker>();   
+    }
     public ArrayList<Worker> getSalary()
     {
-        ArrayList<Worker> workerList = new ArrayList<Worker>();
+     //   workerList = new ArrayList<Worker>();
         for(Worker w: workerReg.getRange(0, workerReg.getCount()))
         {
             //List of all workers in database
@@ -43,26 +54,33 @@ public class SetSalaryBean implements Serializable
         }
         return workerList;    
     }
+    
+    //Save salaries
+    public void onCellEdit(CellEditEvent event)
+    {
+        Object oldValue = event.getOldValue();
+        Object newValue = event.getNewValue();
+        int dataTableIndex = event.getRowIndex();
+        newSalary = (Integer) event.getNewValue();
+        Worker w = workerList.get(dataTableIndex);
+        w.setSalary(newSalary);
+        workerReg.update(w);
+        
+        FacesMessage msg = new FacesMessage("Salary is changed for: ", w.getName()); 
+            FacesMessage msg2 = new FacesMessage("The salary is :" , Integer.toString(newSalary));
+            FacesContext.getCurrentInstance().addMessage(null, msg); 
+            FacesContext.getCurrentInstance().addMessage(null, msg2);
+        
+    }
     //Save salaries
     public void update(RowEditEvent event)
     {
        //      update((Worker)event.getObject());
-           
-                   
-          Worker w = ((Worker)event.getObject());
-          
-          RequestContext requestContext = RequestContext.getCurrentInstance();
-        //  event.
-     //     requestContext.addCallbackParam("rowIndex", event.getRowIndex());
-        //  w.setSalary(1337);
+           w = ((Worker)event.getObject());
+
+          w.setSalary(0);
           workerReg.update(w);
-            // Long pNumber = ((Worker)event.getObject()).getIdNumber();
-            // String name = ((Worker)event.getObject()).getName();
-            // Worker w = workerReg.getById(pNumber);
-        //     int salary = ((Worker)event.getObject()).getSalary();
-      //       w.setSalary(salary);
-           //  w.setSalary(salary);
-     //        workerReg.update(w);
+
             
             FacesMessage msg = new FacesMessage("Salary is changed for: ", ((Worker) event.getObject()).getName()); 
             FacesMessage msg2 = new FacesMessage("The salary is :" , Long.toString(((Worker) event.getObject()).getSalary()));
